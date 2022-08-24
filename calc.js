9d56244400015f26d76d35d536a6546ae9cf9b75
Ashley Pliler <ashleypliler1@gmail.com>
2022-08-23 20:14:52 -0600

    fixed the square root

--- calc.js
+++ calc.js
@@ -14,9 +14,7 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
-console.log('mathSymbol', mathSymbol);
-console.log('num1', num1);
-console.log('num2', num2);
+console.log(mathSymbol, num1, num2);
 
 if (mathSymbol === "+"){
 	console.log(num1 + num2);
@@ -30,8 +28,8 @@ if (mathSymbol === "*"){
 if (mathSymbol === "/"){
 	console.log(num1 / num2);
 };
-if (mathSymbol === "**2"){
-	console.log(num1 **2, num2 **2);
+if (mathSymbol === "math.sqrt"){
+	console.log(Math.sqrt(num1));
 };
 
 	// This line closes the connection to the command line interface.
